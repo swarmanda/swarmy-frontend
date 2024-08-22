e7f53fb457ed2fa1141b41af237bf5fe39c4178a
Mate Smajda <mate.smajda@accretivetg.com>
2024-08-22 22:19:37 +0200

    api guide auto fill key

--- src/routes/ApiGuideRoute.tsx
+++ src/routes/ApiGuideRoute.tsx
@@ -1,31 +1,46 @@
-import { Alert, Space } from '@mantine/core';
+import { Alert, Container, Flex, Select, Space, Stack, Text } from '@mantine/core';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { stackoverflowDark as theme } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { config } from '../config.tsx';
+import { useQuery } from '@tanstack/react-query';
+import { api } from '../api/Api.ts';
+import { useState } from 'react';
 
 export default function ApiGuideRoute() {
   const downloadEndpoint = `${config.apiUrl}/files`;
   const uploadEndpoint = `${config.apiUrl}/api/files`;
+  const [apiKey, setApiKey] = useState<string | null>('YOUR_API_KEY');
+
+  const { isSuccess, data } = useQuery({
+    queryKey: ['api-keys'],
+    queryFn: api.getApiKeys,
+  });
 
   function getBashFileUpload() {
-    return `curl -X POST -F file=@file.jpg -H "Authorization: Bearer YOUR_API_KEY" "${uploadEndpoint}"`;
+    return `curl -X POST -F file=@file.jpg -H "Authorization: Bearer ${apiKey}" "${uploadEndpoint}"`;
   }
 
   function getBashWebsiteUpload() {
-    return `curl -X POST -F file=@site.tar -H "Authorization: Bearer YOUR_API_KEY" "${uploadEndpoint}?website=true"`;
+    return `curl -X POST -F file=@site.tar -H "Authorization: Bearer ${apiKey}" "${uploadEndpoint}?website=true"`;
   }
 
   function getBashFileDownload() {
-    return `curl "${downloadEndpoint}/hash_of_file?k=YOUR_API_KEY --output filename`;
+    return `curl "${downloadEndpoint}/hash_of_file?k=${apiKey} --output filename`;
   }
 
   function getBashListFiles() {
-    return `curl -H "Authorization: YOUR_API_KEY" "${uploadEndpoint}"`;
+    return `curl -H "Authorization: Bearer ${apiKey}" "${uploadEndpoint}"`;
   }
 
   return (
     <>
       <h1>Api guide</h1>
+      <Flex justify={'flex-end'} mr={'lg'}>
+        <Stack gap={'xs'}>
+          <Text>Key to use:</Text>
+          <Select w={250} data={isSuccess ? data.map((d) => d.key) : []} value={apiKey} onChange={setApiKey} />
+        </Stack>
+      </Flex>
 
       <h2>Upload a file</h2>
       <SyntaxHighlighter language="bash" style={theme}>
@@ -38,7 +53,7 @@ export default function ApiGuideRoute() {
         {`import * as fs from 'node:fs';
 import axios from 'axios';
 
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = '${apiKey}';
 const buffer = fs.readFileSync('path/to/file');
 const blob = new Blob([buffer]);
 const file = new File([blob], 'yourFileName.png')
@@ -96,9 +111,7 @@ cd ..`}
       </Alert>
 
       <Space h={'lg'} />
-      <div>
-        Next, simply POST the tar file to the upload endpoint.
-      </div>
+      <div>Next, simply POST the tar file to the upload endpoint.</div>
 
       <SyntaxHighlighter language="bash" style={theme}>
         {getBashWebsiteUpload()}
